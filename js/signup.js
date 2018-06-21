0bd10bf3130d4051ed184c1df8daa342625d00f5
mariakatusiime <mariakatusiime@gmail.com>
2018-06-21 18:27:25 +0300

    just editing

---
+++ js/signup.js
@@ -0,0 +1,25 @@
+<script>
+function signup() {
+if(
+    var fname = document.forms("signupform").("fname").value;
+    var sname = document.forms("signupform").("sname").value;
+    var usname = document.forms("signupform").("usname").value;
+    var pswd = document.forms("signupform").("password").value;
+    var rpdwd = document.forms("signupform").("rpassword").value;
+    var email = document.forms("signupform").("email").value;
+    var dept = document.forms("signupform").("dept").value;
+    var empno = document.forms("signpform").("empno").value; 
+){
+
+    alert("You have been succefully registered")
+    document.getElementById("p").innerHTML = document.getElementById("po");
+    
+
+}
+
+
+
+}
+
+
+</script>
